8e995e6b2f7635bf110d6c80394dd8d199e9bf55
keresztpapa <48327504+keresztpapa@users.noreply.github.com>
2021-12-04 09:39:37 +0100

    Update dhopp.sh

--- dhopp.sh
+++ dhopp.sh
@@ -14,14 +14,14 @@ apt install nmap -y
 apt install ettercap-text-only -y
 check_snap
 snap install discord 
-apt install virtualbox
+apt install virtualbox -y
 apt install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa -y
-apt install python3.8
-apt install python3-pip
-snap install --classic code
-apt-get htop 
-apt install net-tools
+apt install python3.10 -y
+apt install python3-pip -y
+snap install --classic code -y
+apt-get htop -y
+apt install net-tools -y
 
 
 apt-get update
